bdf267dad2b6a293a7c19f99c3f480f41ec29c48
Andrew Caruso <10090913+Yzma@users.noreply.github.com>
2023-03-12 23:33:03 -0400

    Rename to @product variable and text errors array is empty or not

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -10,36 +10,41 @@ RSpec.describe Product, type: :model do
 
     describe "a valid product" do
       it "should save successfully" do
-        @valid_product = Product.new(name: "Test Name", price_cents: 5800, quantity: 54, category: @valid_category)
-        expect(@valid_product).to be_valid
+        @product = Product.new(name: "Test Name", price_cents: 5800, quantity: 54, category: @valid_category)
+        expect(@product).to be_valid
+        expect(@product.errors).to be_empty
       end
     end
 
     describe ":name" do
       it "should not be valid" do
-        @valid_product = Product.new(name: nil, price_cents: 5800, quantity: 54, category: @valid_category)
-        expect(@valid_product).to_not be_valid
+        @product = Product.new(name: nil, price_cents: 5800, quantity: 54, category: @valid_category)
+        expect(@product).to_not be_valid
+        expect(@product.errors).to_not be_empty
       end
     end
 
     describe ":price" do
       it "should not be valid" do
-        @valid_product = Product.new(name: "Test Name", price_cents: nil, quantity: 54, category: @valid_category)
-        expect(@valid_product).to_not be_valid
+        @product = Product.new(name: "Test Name", price_cents: nil, quantity: 54, category: @valid_category)
+        expect(@product).to_not be_valid
+        expect(@product.errors).to_not be_empty
       end
     end
 
     describe ":quantity" do
       it "should not be valid" do
-        @valid_product = Product.new(name: "Test Name", price_cents: 5800, quantity: nil, category: @valid_category)
-        expect(@valid_product).to_not be_valid
+        @product = Product.new(name: "Test Name", price_cents: 5800, quantity: nil, category: @valid_category)
+        expect(@product).to_not be_valid
+        expect(@product.errors).to_not be_empty
       end
     end
 
     describe ":category" do
       it "should not be valid" do
-        @valid_product = Product.new(name: "Test Name", price_cents: 5800, quantity: 54, category: nil)
-        expect(@valid_product).to_not be_valid
+        @product = Product.new(name: "Test Name", price_cents: 5800, quantity: 54, category: nil)
+        expect(@product).to_not be_valid
+        expect(@product.errors).to_not be_empty
       end
     end
 
