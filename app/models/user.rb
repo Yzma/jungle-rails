be9ce3053b27d17607d20aa613cc8792fd7f832f
Andrew Caruso <10090913+Yzma@users.noreply.github.com>
2023-03-13 12:56:52 -0400

    Refactor User model - added authenticate_with_credentials and validate all fields before inserting

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,21 @@
 class User < ApplicationRecord
   has_secure_password
+
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, length: { minimum: 5 }, allow_blank: false
+  validates :password_confirmation, presence: true
+
+  before_save { |user| user.email = user.email.downcase }
+
+  def self.authenticate_with_credentials(email, password)
+    user = User.find_by(email: email.downcase.strip)
+  
+    if(user && user.authenticate(password))
+      return user
+    else
+      return nil
+    end
+  end
+
 end
