91b8041f2eea6137ec6868f0c8e603bb4ccf4952
Andrew Caruso <10090913+Yzma@users.noreply.github.com>
2023-03-13 15:49:35 -0400

    Added more cart tests

--- cypress/integration/add_to_cart.spec.js
+++ cypress/integration/add_to_cart.spec.js
@@ -9,4 +9,12 @@ describe('Add item to cart', () => {
     cy.contains('My Cart (1)');
   });
 
+  it("should add item to the cart and go to cart page to verify it's added", () => {
+    cy.get('.products > :nth-child(1) > div > form > button').click({force: true});
+    
+    cy.visit('http://127.0.0.1:3000/cart');
+
+    cy.contains('My Cart');
+  });
+
 });
