39c1928a7c505ea5949250375f80c7422458def7
Andrew Caruso <10090913+Yzma@users.noreply.github.com>
2023-03-13 22:50:44 -0400

    Implement destory function for products controller and general cleanup

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,25 +1,40 @@
 class ReviewsController < ApplicationController
 
   before_action :authorize
+  before_action :check_null_product_and_return
+
+  def check_null_product_and_return
+    @product = Product.find_by(id: params[:product_id])
+    if @product.blank?
+      return redirect_to '/products'
+    end
+  end
 
   def create
-    if current_user
-      product = Product.find_by(id: params[:product_id])
 
-      review = product.reviews.new(review_params)
-      review.user = current_user
+    review = @product.reviews.new(review_params)
+    review.user = current_user
 
-      if review.save
-        redirect_to "/products/#{params[:product_id]}"
-      else
-        # TODO: Route somewhere else?
-        raise "This wasn't supposed to happen"
-      end
-      
+    if review.save
+      redirect_to @product
     else
-
-      redirect_to "/products/#{params[:product_id]}"
+      # TODO: Route somewhere else?
+      raise "This wasn't supposed to happen"
     end
+  end
+
+  def destroy
+    @review = Review.find_by(id: params[:review_id])
+    if @review.blank?
+      return redirect_to @product
+    end
+
+    if(@review.user.id == current_user.id && @review.destroy)
+      redirect_to @product
+    else
+      raise "Not correct user?"
+    end
+
   end
 
   def review_params
